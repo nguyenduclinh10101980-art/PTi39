873da0e7b6ef43112225da857b1502018547ac2f
nguyenduclinh10101980-art <nguyenduclinh10101980@gmail.com>
2026-09-20 15:56:10 +0700

    Add files via upload

---
+++ checkpoint/json.py
@@ -0,0 +1,17 @@
+[
+    {
+        "name": "Lập trình App Producer",
+        "priority": 3,
+        "completed": True
+    },
+    {
+        "name": "Làm văn",
+        "priority": 2,
+        "completed": True
+    },
+    {
+        "name": "Lập trình GameMaker",
+        "priority": 3,
+        "completed": True
+    }
+]
--- checkpoint/khevai.py
+++ checkpoint/khevai.py
@@ -1,31 +1,35 @@
 import json
-
-class Homework:
-    def __init__(self, name, priority, completed=False):
-        self.name = name
-        self.priority = priority
-        self.completed = completed
+from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6 import uic
 
 
-hw_list = [
-    Homework("Lap trinh App Producer", 3),
-    Homework("Lam van", 2, True),
-    Homework("Lap trinh GameMaker", 3)
-]
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-for item in hw_list:
-    print(item.name, item.priority, item.completed)
+        uic.loadUi("main.ui", self)
 
-data = []
+        with open("new_data.json", "r", encoding="utf-8") as file:
+            self.data = json.load(file)
 
-for item in hw_list:
-    data.append({
-        "name": item.name,
-        "priority": item.priority,
-        "completed": item.completed
-    })
+        self.show_data()
 
-with open("homework.json", "w") as file:
-    json.dump(data, file, indent=4)
+        self.btnComplete.clicked.connect(self.complete)
 
-print("Da luu!")
+    def show_data(self):
+        self.listHomework.clear()
+
+        for item in self.data:
+
+            if item["completed"]:
+                text = " ✅" + item["name"]
+            else:
+                text = "⬜ " + item["name"]
+            self.listHomework.addItem(text)
+        with open("new_data.json", "w", encoding="utf-8") as file:
+            json.dump(self.data, file, ensure_ascii=False, indent=4)
+        self.show_data()
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()
